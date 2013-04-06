b0a02d04112cea878a0781a52389073aba6580a1
Dan Pilone <dan@element84.com>
2013-04-06 10:26:26 -0400

    Initial rails commit

---
+++ streaming_sort/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreamingSort::Application.config.secret_key_base = '617b8f13044cb01eca0fd2e79718f014d620071f388e8bb79e88fdc0ed4eb7947d60428e59106f191c2f3978b8e6780d1f83d736f87ce81497d106556e84513a'
---
+++ streaming_sort/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreamingSort::Application.config.session_store :encrypted_cookie_store, key: '_streaming_sort_session'

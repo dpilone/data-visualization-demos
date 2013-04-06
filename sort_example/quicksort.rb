1623906967856bb788b9a2e371b25df51e748683
Dan Pilone <dan@element84.com>
2013-04-06 10:23:44 -0400

    Refactored to use more obvious argument names.

--- sort_example/quicksort.rb
+++ sort_example/quicksort.rb
@@ -1,43 +1,28 @@
 # quicksort.rb
 # From: http://opennfo.wordpress.com/2009/02/15/quicksort-in-ruby/
 
-def quicksort(f, list, p, r)
-    if p < r then
-        q = partition(list, p, r)
-        f.puts "<tr><td>#{list}</td><td>#{p}</td><td>#{r}</td></tr>"
-        quicksort(f, list, p, q-1)
-        quicksort(f, list, q+1, r)
+def quicksort(list, left_index, right_index)
+    if left_index < right_index then
+        q = partition(list, left_index, right_index)
+        quicksort(list, left_index, q-1)
+        quicksort(list, q+1, right_index)
     end
 end
 
-def partition(list, p, r)
-    pivot = list[r]
-    i = p - 1
-    p.upto(r-1) do |j|
+def partition(list, left_index, right_index)
+    pivot = list[right_index]
+    i = left_index - 1
+    left_index.upto(right_index-1) do |j|
         if list[j] <= pivot
             i = i+1
             list[i], list[j] = list[j],list[i]
         end
     end
-    list[i+1],list[r] = list[r],list[i+1]
+    list[i+1],list[right_index] = list[right_index],list[i+1]
     return i + 1
 end
 
 # Testing it out
-f = File.new('quicksort.html', 'w')
-f.puts "<!DOCTYPE html>"
-f.puts "<html>"
-f.puts "<head>"
-f.puts "<link href='bootstrap/css/bootstrap.min.css' rel='stylesheet' media='screen'>"
-f.puts "</head>"
-f.puts "<body>"
-f.puts "<table class='table table-hover table-bordered'>"
-f.puts "<thead><tr><th>List</th><th>p</th><th>r</th></tr></thead>"
-f.puts "<tbody>"
-#a = [9,4,10,12,3,5,10,3,2,25,6,21,33,23,19,13,38,26,12,3]
-a = [5,4,3,2,1]
-quicksort(f, a, 0, a.length-1)
-f.puts "</tbody>"
-f.puts "</body>"
-f.puts "</table>"
-puts a
+a = [9,4,10,12,5,3,2,25,6,21,33,23,19,13,38,26]
+quicksort(a, 0, a.length-1)
+puts a.inspect
